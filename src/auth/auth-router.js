8d2312b51e29091cbe3ae26899985f073d781f9e
Danielle Marie <dhutchings36@yahoo.com>
2020-02-12 08:14:49 -0600

    fixed response message

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -10,7 +10,7 @@ authRouter
     for (const [key, value] of Object.entries(loginUser))
       if (value == null)
         return res.status(400).json({
-          error: `Must include all fields`
+          error: `Missing '${key}' in request body`
         })
 
     AuthService.getUserByUsername(
