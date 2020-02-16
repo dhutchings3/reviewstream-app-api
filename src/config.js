7b465a7cd2da8c018845427632321ebb9a58f861
Danielle Marie <dhutchings36@yahoo.com>
2020-02-16 11:09:29 -0600

    updated for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/reviewstream',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/reviewstream-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/reviewstream',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/reviewstream-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
   }
