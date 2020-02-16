70bb804980650ad625b49b9adf0cca49cce0c22f
Danielle Marie <dhutchings36@yahoo.com>
2020-02-16 12:30:46 -0600

    working on deployment

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/reviewstream',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/reviewstream-test',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://reviewstream-app.dhutchings36.now.sh/',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
   }
